4ece8d64f95b522f619c28a0cb4d4564bb9b41dc
niC00L <nika.blanarova@gmail.com>
2019-11-27 18:10:04 +0100

    power up manager

--- Assets/PowerUps/PowerUpManager.cs
+++ Assets/PowerUps/PowerUpManager.cs
@@ -4,19 +4,14 @@ using UnityEngine;
 
 public class PowerUpManager : MonoBehaviour
 {
-    public GameObject powerUp;                // The enemy prefab to be spawned.
-    public float spawnTime = 10f;            // How long between each spawn.
-    public Transform[] spawnPoints;         // An array of the spawn points this enemy can spawn from.
-
-    // Start is called before the first frame update
+    public GameObject powerUp;                
+    public Transform[] spawnPoints;         
+    
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        for (int i = 0; i < spawnPoints.Length; i++)
+        {
+            Instantiate(powerUp, spawnPoints[i].position, spawnPoints[i].rotation);
+        }
     }
 }
